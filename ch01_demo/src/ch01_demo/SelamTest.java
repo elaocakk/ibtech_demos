d8cf0da16cdc2bf44db35be68f9f6fb85a95978f
elaocak <49361580+elaocakk@users.noreply.github.com>
2022-10-23 21:04:09 +0300

    array demo

--- ch01_demo/src/ch01_demo/SelamTest.java
+++ ch01_demo/src/ch01_demo/SelamTest.java
@@ -6,32 +6,47 @@ public class SelamTest {
 	
 	public static void main(String[] args) {
 		
-		Selam nesne = new Selam();		
-		String cevap = nesne.helloEverybody("Ela");
-		System.out.println(cevap);
-		
-		String cevap2 = nesne.helloEverybody("Eda");
-		System.out.println(cevap2);
-		
-		System.out.println("test" + "'");
-		
-		System.out.println("test2" + '\'');
-		
-		System.out.println(); // ctrl + space
-		
-		Selam nesnetest = new Selam();		
-		String cevaptest = nesnetest.helloEverybody("Ela");
-		System.out.println(cevaptest);
-		
+//		Selam nesne = new Selam();		
+//		String cevap = nesne.helloEverybody("Ela");
+//		System.out.println(cevap);
+//		
+//		String cevap2 = nesne.helloEverybody("Eda");
+//		System.out.println(cevap2);
+//		
+//		System.out.println("test" + "'");
+//		
+//		System.out.println("test2" + '\'');
+//		
+//		System.out.println(); // ctrl + space
+//		
+//		Selam nesnetest = new Selam();		
+//		String cevaptest = nesnetest.helloEverybody("Ela");
+//		System.out.println(cevaptest);
+//		
 //		String cevap2test = nesnetest.helloEverybody("Eda");
 //		System.out.println(cevap2test);
 		
 		 
-		System.out.println("Bir isim giriniz:");
-		Scanner in = new Scanner(System.in);		
-		String yeniIsim = in.nextLine();
-		String cevaptest2 = nesnetest.helloEverybody(yeniIsim);
-		System.out.println(cevaptest2);
+//		System.out.println("Bir isim giriniz:");
+//		Scanner in = new Scanner(System.in);		
+//		String yeniIsim = in.nextLine();
+//		String cevaptest2 = nesnetest.helloEverybody(yeniIsim);
+//		System.out.println(cevaptes
+		
+		// array demo
+	    int [] intArray = new int [5];
+	    int i, j;
+	    
+	    for(i=0 ; i < intArray.length ; i++ ) {	    	
+	    	intArray[i] = i*i;	    	
+	    }
+	    
+	    for(j=0 ; j < intArray.length ; j++ ) {
+	    	System.out.println(intArray[j]);	    	
+	    }
+	 
+	   
+		
 		
 	}
 	
